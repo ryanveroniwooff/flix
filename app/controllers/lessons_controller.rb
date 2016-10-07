259d7dd092c7c7bda73f7dd348db3e656f0ce893
Ryan Wooff <ryanwooff@gmail.com>
2016-10-07 01:58:54 +0000

    bug hunting: 1.1

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,13 +1,11 @@
 class LessonsController < ApplicationController
   before_action :authenticate_user!
   def show
-    @lesson = current_lesson.section.course(@course)
-    if !current_lesson.section.course.user.enrolled_in?(course_url)
-      redirect_to current_lesson
-    else
-      redirect_to course_path(course_url), alert: 'You must be enrolled in this course!'
+    if !current_lesson.section.course.user.enrolled_in?(course_url) && current_lesson.section.course.user != current_user
+      redirect_to course_path(current_lesson.section.course), alert: 'You must be enrolled in this course!'
     end
   end
+
 
   private
     helper_method :current_lesson
