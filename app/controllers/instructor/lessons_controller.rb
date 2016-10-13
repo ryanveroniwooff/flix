82b68e7f1c42b23474b0c6f5bf061317ae4eb78b
Ryan Wooff <ryanwooff@gmail.com>
2016-10-13 05:16:25 +0000

    Added Drag/Drop JS Feature #2

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -3,43 +3,47 @@ class Instructor::LessonsController < ApplicationController
   before_action :require_authorized_for_current_section, only: [:new, :create]
   before_action :require_authorized_for_current_lesson, only: [:update]
 
-
   def new
-    current_lesson = Lesson.new
+    @lesson = Lesson.new
   end
 
   def create
-    current_lesson = current_section.lessons.create(lesson_params)
+    @lesson = current_section.lessons.create(lesson_params)
     redirect_to instructor_course_path(current_section.course)
   end
+
   def update
     current_lesson.update_attributes(lesson_params)
-    render text: 'Updated Successfully!'
+    render text: 'Updated!'
   end
 
   def video
     @video = video
   end
 
+
+
   private
+
   def require_authorized_for_current_lesson
     if current_lesson.section.course.user != current_user
       render text: 'Unauthorized', status: :unauthorized
     end
   end
 
+  def current_lesson
+    @current_lesson ||= Lesson.find(params[:id])
+  end
+
   def require_authorized_for_current_section
     if current_section.course.user != current_user
-      return render text: 'Unauthorized', status: :unauthorized
+      render text: 'Unauthorized', status: :unauthorized
     end
   end
 
   helper_method :current_section
   def current_section
     @current_section ||= Section.find(params[:section_id])
-  end
-  def current_lesson
-    @current_lesson ||= Lesson.find(params[:id])
   end
 
   def lesson_params
